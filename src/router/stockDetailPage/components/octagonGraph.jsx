82c77a5766e67f29ece642834a530ded8e993038
jiho <unit013@naver.com>
2023-10-18 21:53:49 +0900

    add graph

--- src/router/stockDetailPage/components/octagonGraph.jsx
+++ src/router/stockDetailPage/components/octagonGraph.jsx
@@ -1,3 +1,43 @@
+import { ResponsiveRadar} from '@nivo/radar'
+import { pentagonData } from '../../../dataSet';
+
 export default function OctagonGraph() {
-  return <></>;
+  return(
+    <ResponsiveRadar
+        data={pentagonData}
+        keys={[ 'now', '6 month ago', '1 year ago' ]}
+        indexBy="taste"
+        valueFormat=">-.2f"
+        margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
+        borderColor={{ from: 'color' }}
+        gridLabelOffset={36}
+        dotSize={0}
+        dotColor={{ theme: 'background' }}
+        dotBorderWidth={2}
+        colors={{ scheme: 'category10' }}
+        blendMode="multiply"
+        motionConfig="wobbly"
+        legends={[
+            {
+                anchor: 'top-left',
+                direction: 'column',
+                translateX: -50,
+                translateY: -40,
+                itemWidth: 80,
+                itemHeight: 20,
+                itemTextColor: '#999',
+                symbolSize: 12,
+                symbolShape: 'circle',
+                effects: [
+                    {
+                        on: 'hover',
+                        style: {
+                            itemTextColor: '#000'
+                        }
+                    }
+                ]
+            }
+        ]}
+    />
+  );
 }
